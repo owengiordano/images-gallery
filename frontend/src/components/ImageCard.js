633212cfef3469caaede140b6f3a490eec9a68d2
Owen Giordano <owengiord@gmail.com>
2023-06-05 15:22:58 -0400

    changed color scheme of texts

--- frontend/src/components/ImageCard.js
+++ frontend/src/components/ImageCard.js
@@ -11,7 +11,7 @@ const ImageCard = ({ image, deleteImage }) => {
   return (
     <Card style={{ width: "18rem" }}>
       <Card.Img variant="top" src={image.urls.small} />
-      <Card.Body>
+      <Card.Body style={{ color: "#4d695d" }}>
         <Card.Title>{image.title.toUpperCase()}</Card.Title>
         <Card.Text>{image.description || image.alt_description}</Card.Text>
         <Button
--- frontend/src/components/Welcome.js
+++ frontend/src/components/Welcome.js
@@ -15,7 +15,7 @@ const divStyle = {
 const Welcome = () => {
   return (
     <div style={divStyle} className="p-5 text-center">
-      <h1>Images Gallery</h1>
+      <h1>Welcome!</h1>
       <p>
         This is a simple application that retrieves photos using Unsplash API.
         In order to start enter any search term in the input field.
