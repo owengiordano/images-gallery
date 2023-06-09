a22cacdbe8cf8b9ccd436aa288d8c35bd9b3516d
Owen Giordano <owengiord@gmail.com>
2023-06-08 22:41:42 -0400

    pymongo and mongo test

---
+++ api/mongo_client.py
@@ -0,0 +1,28 @@
+import os
+from dotenv import load_dotenv
+from pymongo import MongoClient
+
+load_dotenv(dotenv_path="./.env.local")
+
+MONGO_URL = os.environ.get("MONGO_URL", "mongo")
+MONGO_USERNAME = os.environ.get("MONGO_USERNAME", "root")
+MONGO_PASSWORD = os.environ.get("MONGO_PASSWORD", "")
+MONGO_PORT = os.environ.get("MONGO_PORT", 27017)  # 27017 is default port
+
+
+mongo_client = MongoClient(
+    host=MONGO_URL,
+    username=MONGO_USERNAME,
+    password=MONGO_PASSWORD,
+    port=MONGO_PORT,
+)
+
+
+def insert_test_document():
+    # Inserts sample document to the test_collection in the db test
+    db = mongo_client.test
+    test_collection = db.test_collection
+    res = test_collection.insert_one({"name": "Owen1 Giordano", "teacher": "Gio1"})
+    res = test_collection.insert_one({"name": "Owen2 Giordano", "teacher": "Gio2"})
+
+    print(res)
