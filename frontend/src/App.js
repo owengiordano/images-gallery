348cc2f4d380cbcfb7c2ca44b26965e4c6260431
Owen Giordano <owengiord@gmail.com>
2023-06-09 00:10:15 -0400

    introduce axios and rewrite fetch GET call

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Search from "./components/Search";
 import ImageCard from "./components/ImageCard";
 import { Container, Row, Col } from "react-bootstrap";
 import Welcome from "./components/Welcome";
+import axios from "axios";
 
 //const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 const API_URL = process.env.REACT_APP_API_URL || "http://127.0.0.1:5050";
@@ -13,16 +14,15 @@ function App() {
   const [word, setWord] = useState("");
   const [images, setImages] = useState([]);
 
-  const handleSearchSubmit = (e) => {
+  const handleSearchSubmit = async (e) => {
     e.preventDefault();
-    fetch(`${API_URL}/new-image?query=${word}`)
-      .then((res) => res.json())
-      .then((data) => {
-        setImages([{ ...data, title: word }, ...images]);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+
+    try {
+      const res = await axios.get(`${API_URL}/new-image?query=${word}`);
+      setImages([{ ...res.data, title: word }, ...images]);
+    } catch (error) {
+      console.log(error);
+    }
     setWord("");
   };
 
